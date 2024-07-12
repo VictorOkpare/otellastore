893553c76201ac343b823328fa97bb772de49b39
VictorOkpare <victorokpare@gmail.com>
2024-07-12 07:49:06 +0100

    Hamburger menu created, data added to roundneckscreen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,144 +1,139 @@
-import React from 'react'
-import { Link  } from 'react-router-dom'
-import { IoMdSearch } from 'react-icons/io'
-import { MdOutlineLightMode } from "react-icons/md"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { IoMdSearch } from 'react-icons/io';
 import { GiShoppingCart } from "react-icons/gi";
-
-import Darkmode from './Darkmode'
-import { FaRegUser, FaCaretDown } from "react-icons/fa";
-
-
+import Darkmode from './Darkmode';
+import { FaRegUser, FaCaretDown, FaTimes, FaBars } from "react-icons/fa";
 
 const Menu = [
-  {
-    id:1,
-    name: "Home",
-    link: "/#"
-  },
-
-  {
-    id:2,
-    name:"Customization",
-    link: "/#"
-  },
-  {
-    id:3,
-    name: 'Order Tracking',
-    link: "/orderTracking", 
-
-
-
-  },
-  {
-    id:4,
-    name: 'Contact',
-    link: "/Contact", 
-
-
-
-  },
-]
+  { id: 1, name: "Home", link: "/#" },
+  { id: 2, name: "Customization", link: "/#" },
+  { id: 3, name: 'Order Tracking', link: "/orderTracking" },
+  { id: 4, name: 'Contact', link: "/Contact" },
+];
 
 const DropdownLinks = [
-  {
-    id:1,
-    name: "Roundneck Tshirts",
-    link: "/Roundneck"
-  },
-  {
-    id:2,
-    name: "Polo Tshirts",
-    link: "/#"
-  },
-  {
-    id:3,
-    name: "V-neck Tshirts",
-    link: "/#"
-  },
-  {
-    id:4,
-    name: "Kids Tshirts",
-    link: "/#"
-  }
-]
+  { id: 1, name: "Roundneck Tshirts", link: "/Roundneck" },
+  { id: 2, name: "Polo Tshirts", link: "/#" },
+  { id: 3, name: "V-neck Tshirts", link: "/#" },
+  { id: 4, name: "Kids Tshirts", link: "/#" },
+];
 
-const Navbar = ({handleOrderPopup}) => {
+const Navbar = ({ handleOrderPopup }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => setOpen((prev) => !prev);
+
   return (
     <div className='shadow-md bg-white dark:bg-black dark:text-white duration-200 relative z-40'>
-        {/* upper Navbar */}
+      {/* Upper Navbar */}
       <div className='bg-white dark:bg-black py-2'>
         <div className='container flex justify-between items-center'>
-            <div> 
-                <Link to='#' className=' font-extrabold w-10   flex gap-2 text-black font-robotoCondensed text-lg dark:text-white'>
-               <span className='bg-orange-500 p-2 text-white rounded-full font-nunito'>Otella<span className=' text-orange-500 px-1 bg-white rounded-full'>store</span></span>
-                </Link>
-            </div>
-            {/* search bar */}
-            <div className='flex justify-between items-center gap-4'>
-                <div className='group relative hidden sm:block'>
-                    <input type="text"
-                    placeholder='search ...'
-                    className='w-[200px] sm:w-[200px] group-hover:w-[300px] translate-all duration-300 rounded-full border border-black px-2 py-1 focus:outline-none focus:border-1 focus:border-black dark:border-gray-500 dark:bg-gray-800' />
-                    <IoMdSearch className='text-black text-lg group-hover:text-orange-500  absolute top-1/2 -translate-y-1/2 right-3'/>
-                </div>
-            </div>
-            <div className='flex gap-8'>
-            {/* order button */}
-            <button className='bg-orange-500 transition-all duration-200 text-white dark:text-black font-bold py-1 px-4 rounded-full flex items-center gap-1 group dark:bg-orange-500'>
-                <span
-                className='group-hover:block hidden transition-all duration-200 '>Cart</span>
-                <GiShoppingCart className='text-2xl  text-white dark:text-black drop-shadow-sm cursor-pointer '/>
-
-            </button>
-            {/* login */}
-            <div>
-              <button onClick={()=> handleOrderPopup()} className='bg-orange-500 transition-all duration-200 text-white dark:text-black font-bold py-1 px-4 rounded-full flex items-center gap-3 group dark:bg-orange-500'>
-                <span className='group-hover:block hidden transition-all duration-10000 '  >Sign-up</span>
-                <FaRegUser className='text-xl cursor-pointer drop-shadow-sm  '/>
+          <div className='flex items-center'>
+            {/* Hamburger button */}
+            <div className='flex md:hidden'>
+              <button
+                type='button'
+                onClick={handleMenu}
+                className='flex justify-center p-2 rounded-md text-orange-500'
+              >
+                <span className='sr-only'>Open Main Menu</span>
+                {open ? <FaTimes /> : <FaBars />}
               </button>
             </div>
-            
-            {/* darkmode switch */}
+
+            {/* Logo */}
             <div>
-            <Darkmode className='text-2xl'/>
+              <Link
+                to='#'
+                className='font-extrabold w-10 flex gap-2 text-black font-robotoCondensed text-lg dark:text-white'
+              >
+                <span className='bg-orange-500 p-2 text-white rounded-full font-nunito'>
+                  Otella<span className='text-orange-500 px-1 bg-white rounded-full'>store</span>
+                </span>
+              </Link>
             </div>
+          </div>
+
+          {/* Search bar */}
+          <div className='flex justify-between items-center gap-4'>
+            <div className='group relative hidden sm:block'>
+              <input
+                type="text"
+                placeholder='Search ...'
+                className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-black px-2 py-1 focus:outline-none focus:border-1 focus:border-black dark:border-gray-500 dark:bg-gray-800'
+              />
+              <IoMdSearch className='text-black text-lg group-hover:text-orange-500 absolute top-1/2 -translate-y-1/2 right-3' />
+            </div>
+          </div>
+
+          <div className='flex gap-8'>
+            {/* Cart button */}
+            <button className='bg-orange-500 transition-all duration-200 text-white dark:text-black font-bold py-1 px-4 rounded-full flex items-center gap-1 group dark:bg-orange-500'>
+              <span className='group-hover:block hidden transition-all duration-200'>Cart</span>
+              <GiShoppingCart className='text-2xl text-white dark:text-black drop-shadow-sm cursor-pointer' />
+            </button>
+            
+            {/* Login button */}
+            <button onClick={handleOrderPopup} className='bg-orange-500 transition-all duration-200 text-white dark:text-black font-bold py-1 px-4 rounded-full flex items-center gap-3 group dark:bg-orange-500'>
+              <span className='group-hover:block hidden transition-all duration-200'>Sign-up</span>
+              <FaRegUser className='text-xl cursor-pointer drop-shadow-sm' />
+            </button>
+            
+            {/* Dark mode switch */}
+            <Darkmode className='text-2xl' />
+          </div>
         </div>
       </div>
-      </div>
-     
-        {/* lower Navbar */}
-      <div data-aos="zoom-in" className='flex justify-center bg-orange-500   text-white font-md text-sm font-robotoCondensed
-       '>
-        <ul className='sm:flex hidden items-center gap-5 flex-wrap'>
-          {
-            Menu.map((data)=>(
-              <li key={data.id}>
-                <Link className='inline-block px- hover:font-extrabold duration-1000 ' to={data.link}>{data.name}{data.icon}</Link>
-              </li>
-            ))
-          }
-          {/* simple dropdown and links */}
-          <li className='group relative cursor-pointer hover:text-orange-500'>
-            <Link to='#' className='flex items-center gap-[2px] py-2'>Trending Products <span>
-              <FaCaretDown className='transition-all duration-200 group-hover:rotate-180'/>
-              </span></Link>
-              <div className='absolute z-[1000] hidden group-hover:block w-[150px] rounded-md bg-black p-2 text-white shadow-md'>
-                <ul>
-                  {DropdownLinks.map((data)=>(
-                    <li key={data.id}>
-                      <Link to={data.link}
-                      className="inline-block w-full rounded-md p-2 hover:bg-orange-500">{data.name}</Link>
-                    </li>
-                  )
-                )}
 
-                </ul>
-              </div>
+      {/* Mobile Menu */}
+      {open && (
+        <div className='md:hidden '>
+          <ul className='flex flex-col items-center gap-5'>
+            {Menu.map((data) => (
+              <li key={data.id}>
+                <Link className='inline-block px-2 hover:font-extrabold duration-1000' to={data.link}>{data.name}</Link>
+              </li>
+            ))}
+            {/* Render DropdownLinks directly in the mobile menu */}
+            {DropdownLinks.map((data) => (
+              <li key={data.id}>
+                <Link className='inline-block px-2 hover:font-extrabold duration-1000' to={data.link}>{data.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {/* Lower Navbar */}
+      <div className='flex justify-center bg-orange-500 text-white font-md text-sm font-robotoCondensed'>
+        <ul className='md:flex hidden items-center gap-5 flex-wrap'>
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <Link className='inline-block px-2 hover:font-extrabold duration-1000' to={data.link}>{data.name}</Link>
+            </li>
+          ))}
+          {/* Simple dropdown and links */}
+          <li className='group relative cursor-pointer hover:text-orange-500'>
+            <Link to='#' className='flex items-center gap-[2px] py-2'>
+              Trending Products <FaCaretDown className='transition-all duration-200 group-hover:rotate-180' />
+            </Link>
+            <div className='absolute z-[1000] hidden group-hover:block w-[150px] rounded-md bg-black p-2 text-white shadow-md'>
+              <ul>
+                {DropdownLinks.map((data) => (
+                  <li key={data.id}>
+                    <Link to={data.link} className="inline-block w-full rounded-md p-2 hover:bg-orange-500">{data.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+
--- src/screens/ProductScreen/Roundneck.jsx
+++ src/screens/ProductScreen/Roundneck.jsx
@@ -1,39 +1,69 @@
-
-import React, {useState} from 'react'
-import Imagedata from './Roundneck.json'
-
+import React, { useState } from 'react';
+import Imagedata from './Roundneck.json';
 
 function Roundneck() {
+    const [selectedItem, setSelectedItem] = useState(Imagedata[0]);
 
-    const [bigImage, setBigImage] = useState(Imagedata[0].img);
-    const handleImageClick = (img) => {
-      setBigImage(img);
-    }
+    const handleImageClick = (item) => {
+        setSelectedItem(item);
+    };
 
-  return (
-    <>
-        <div className='flex w-[1000px] mx-20px m-auto'>
-            {/* Big Image Roundneck */}
-            <div className=' w-[250px]'>
-                <img src={bigImage} alt="" className='w-inherit' />
-            {/* small Image Roundneck */}
-                <div className='flex  justify-between w-full mt-[15px] '>
-                    {Imagedata.map((data)=>(
+    const handleColorChange = (event) => {
+        const selectedColor = event.target.value;
+        const newItem = Imagedata.find(item => item.colour === selectedColor);
+        if (newItem) {
+            setSelectedItem(newItem);
+        }
+    };
 
-                    <div key={data.id} className='w-[50px] '>
-                        <img src={data.img} alt="" className="w-inherit cursor-pointer"
-                            onClick={() => handleImageClick(data.img)} />
+    return (
+        <>
+            <div className='flex w-[1000px] mx-20px m-auto'>
+                {/* Big Image Roundneck */}
+                <div className='w-[250px]'>
+                    <img src={selectedItem.img} alt="" className='w-inherit' />
+                    {/* Small Images Roundneck */}
+                    <div className='flex justify-between w-full mt-[15px]'>
+                        {Imagedata.map((item) => (
+                            <div key={item.id} className='w-[50px] overflow-hidden border-[1.5px] border-solid'>
+                                <img
+                                    src={item.img}
+                                    alt=""
+                                    className='transition-all duration-[0.3s] ease-linear w-inherit cursor-pointer hover:scale-110'
+                                    onClick={() => handleImageClick(item)}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                        
-           ))}
-           </div>
-
+                {/* Product details section */}
+                <div>
+                    <div className='pname'>{selectedItem.title}</div>
+                    <div>{selectedItem.colour}</div>
+                    <form action="#">
+                        <label htmlFor="colours">Select a different colour</label>
+                        <select name="colours" id="colours" onChange={handleColorChange}>
+                            {Imagedata.map((data) => (
+                                <option value={data.colour} key={data.id}>{data.colour}</option>
+                            ))}
+                        </select>
+                    </form>
+                    <div>
+                        {selectedItem.brand.map((brand, index) => (
+                            <div key={index}>
+                                <div>{brand.name}</div>
+                                <div>
+                                    {brand.size.map((size, index) => (
+                                        <div key={index}>{size.name}</div>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-        
-        
-        </div>
-    </>
-  )
+            </div>
+        </>
+    );
 }
 
-export default Roundneck
+export default Roundneck;
